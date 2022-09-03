8c8900dd59e07926cb6e7905a74880a98750f26e
Manshy <abdelrazek_mansh1@hotmail.com>
2022-09-03 10:36:20 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,142 @@
+/*
+    zdiscord - by - Manshy - License: CC BY-NC-SA 4.0
+    Docs for this file available at or in
+*/
+
+
+/** ******************************
+ * GENERAL CONFIGURATION SETTINGS
+ ********************************/
+
+
+ const LanguageLocaleCode = "ar";
+
+ // PUBLIC VALUES
+ const FiveMServerName = "QB Test";
+ const DiscordInviteLink = "https://discord.gg/RmJXfQKQ4j";
+ const FiveMServerIP = "127.0.0.1";
+ 
+ // This spams the console, only enable for testing if needed
+ const DebugLogs = false;
+ 
+ 
+ /** ********************
+  * DISCORD BOT SETTINGS
+  ***********************/
+ 
+ const EnableDiscordBot = true;
+ 
+ // DISCORD BOT
+ const DiscordBotToken = "MTAxNTM5OTE4OTM5NzY1MTUwNg.G7a3Uu.mSLxbUUxoQ2amH60o8Tn9ZKk_GD2zihd2TmSHc";
+ const DiscordGuildId = "996850348364939335";
+ 
+ // STAFF CHAT
+ const EnableStaffChatForwarding = false;
+ const DiscordStaffChannelId = "1013866316354039858";
+ const AdditionalStaffChatRoleIds = [
+     // "000000000000000",
+ ];
+ 
+ // WHITELISTING / ALLOWLISTING
+ const EnableWhitelistChecking = true;
+ const DiscordWhitelistRoleIds = "1015483155362369586";
+ 
+ // SLASH COMMANDS / DISCORD PERMISSIONS
+ const EnableDiscordSlashCommands = true;
+ const DiscordModRoleId = "1015483795627048990";
+ const DiscordAdminRoleId = "1015483795627048990";
+ const DiscordGodRoleId = "1015483795627048990";
+ 
+ // DISCORD BOT STATUS
+ const EnableBotStatusMessages = true;
+ const BotStatusMessages = [
+     "{servername}",
+     "{playercount} online",
+ ];
+
+// ACE PERMISSIONS
+const EnableAutoAcePermissions = false;
+const AutoAcePermissions = {
+    // "example": "000000000000000000",
+    // "example2": [ "000000000000000000", "000000000000000000"],
+};
+
+// Other
+const SaveScreenshotsToServer = false;
+
+
+/** ************************
+ * WEBHOOK LOGGING SETTINGS
+**************************/
+
+const EnableLoggingWebhooks = false;
+const LoggingWebhookName = "zLogs";
+// put "&" in front of the id if you're to ping a role
+const LoggingAlertPingId = "&000000000000000000";
+// example: "bank": "https://discord.com/webhook/...",
+const LoggingWebhooks = {
+    "example": "https://discord.com/api/webhooks/000000000/sEcRRet-ToK3n_5tUfF_tH8t_YUo-S40u1d-n07-sHar3",
+};
+
+
+/** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !! DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING !!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+module.exports = {
+    EnableDiscordBot: getConBool("discord_enable_bot", EnableDiscordBot),
+    EnableStaffChatForwarding: getConBool("discord_enable_staff_chat", EnableStaffChatForwarding),
+    EnableLoggingWebhooks: getConBool("discord_enable_logging_webhooks", EnableLoggingWebhooks),
+    DebugLogs: getConBool("discord_debug", DebugLogs),
+    DiscordBotToken: GetConvar("discord_token", DiscordBotToken),
+    DiscordGuildId: GetConvar("discord_guild_id", DiscordGuildId),
+    LanguageLocaleCode: GetConvar("discord_lang", LanguageLocaleCode),
+    FiveMServerName: GetConvar("discord_server_name", FiveMServerName),
+    DiscordInviteLink: GetConvar("discord_invite", DiscordInviteLink),
+    FiveMServerIP: GetConvar("discord_server_ip", FiveMServerIP),
+    EnableWhitelistChecking: getConBool("discord_enable_whitelist", EnableWhitelistChecking),
+    DiscordWhitelistRoleIds: getConList("discord_whitelist_roles", DiscordWhitelistRoleIds),
+    EnableDiscordSlashCommands: getConBool("discord_enable_commands", EnableDiscordSlashCommands),
+    DiscordModRoleId: GetConvar("discord_mod_role", DiscordModRoleId),
+    DiscordAdminRoleId: GetConvar("discord_admin_role", DiscordAdminRoleId),
+    DiscordGodRoleId: GetConvar("discord_god_role", DiscordGodRoleId),
+    EnableBotStatusMessages: getConBool("discord_enable_status", EnableBotStatusMessages),
+    BotStatusMessages: BotStatusMessages,
+    EnableAutoAcePermissions: getConBool("discord_enable_ace_perms", EnableAutoAcePermissions),
+    AutoAcePermissions: AutoAcePermissions,
+    SaveScreenshotsToServer: getConBool("discord_save_screenshots", SaveScreenshotsToServer),
+    DiscordStaffChannelId: GetConvar("discord_staff_channel_id", DiscordStaffChannelId),
+    LoggingWebhooks: LoggingWebhooks,
+    LoggingAlertPingId: GetConvar("discord_logging_ping_id", LoggingAlertPingId),
+    LoggingWebhookName: GetConvar("discord_logging_name", LoggingWebhookName),
+    StaffChatRoleIds: [
+        GetConvar("discord_mod_role", DiscordModRoleId),
+        GetConvar("discord_admin_role", DiscordAdminRoleId),
+        GetConvar("discord_god_role", DiscordGodRoleId),
+        ...AdditionalStaffChatRoleIds,
+    ],
+};
+
+/** Returns convar or default value fixed to a true/false boolean
+ * @param {boolean|string|number} con - Convar name
+ * @param {boolean|string|number} def - Default fallback value
+ * @returns {boolean} - parsed bool */
+function getConBool(con, def) {
+    if (typeof def == "boolean") def = def.toString();
+    const ret = GetConvar(con, def);
+    if (typeof ret == "boolean") return ret;
+    if (typeof ret == "string") return ["true", "on", "yes", "y", "1"].includes(ret.toLocaleLowerCase().trim());
+    if (typeof ret == "number") return ret > 0;
+    return false;
+}
+
+/** returns array of items or default array provided
+ * @param {string} con - string of comma separated values
+ * @param {string|Array} def - string of comma separated values
+ * @returns {object} - array of discord ids */
+function getConList(con, def) {
+    const ret = GetConvar(con, def);
+    if (typeof ret == "string") return ret.replace(/[^0-9,]/g, "").replace(/(,$)/g, "").split(",");
+    if (Array.isArray(ret)) return ret;
+    if (!ret) return [];
+}
